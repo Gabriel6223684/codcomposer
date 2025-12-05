dfb58e8e9129825732c19f9b0a52f70d120d9039
Gabriel6223684 <gabrielgb.luiz000@gmail.com>
2025-12-04 20:48:51 -0400

    C

--- html/index.php
+++ html/index.php
@@ -1,6 +1,4 @@
 <?php
-echo 'oi';
-die;
 
 use Slim\Factory\AppFactory;
 
--- instal.sh
+++ instal.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+cd /home/ventreminex
+
 rm -R /vendor
+rm -R composer.lock
 
 composer install --no-dev --no-progress -a
-
-sudo -u postgres psql
+composer update --no-dev --no-progress -a
+composer upgrade --no-dev --no-progress -a
+composer do -o --no-dev --no-progress -a
 
 service nginx reload
